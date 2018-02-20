fa8d2ba274e1e93e19592edde0dc7e64f8693153
Chema Rivera <chema@iMac-de-Chema.local>
2018-02-20 15:50:42 +0100

    some html improvements and advanced into the population task

--- client/js/03_printersPopulation.js
+++ client/js/03_printersPopulation.js
@@ -4,10 +4,8 @@ $(function() {
     $.ajax({
         url: 'http://'+serverAddress+':'+serverPort+'/printers/list',
         method: 'GET',
-        statusCode: {
-        },
         success: function(res, status) {
-            console.log(res);
+            poblateViews(res);
         },
         error: function(jqXHR, status, err) {
             if (!err) {
@@ -19,4 +17,94 @@ $(function() {
             }
         }
     });
+
+
+    function poblateViews(printersList) {
+        var iconsViewPrinters = '<div class="wrapper">';
+        var listViewPrinters = '<table class="wrapper">';
+        var columnsViewPrinters = '<div class="wrapper">';
+        var title = '';
+
+        for (var i=0; i<printersList.length; i++) {
+            iconsViewPrinters += '<div class="printer';
+
+            switch (printersList[i].detailedInfo.status.toLowerCase()) {
+                case 'awake':
+                    iconsViewPrinters += ' connected';
+                    title = 'Awake';
+                    break;
+                case 'sleep':
+                    iconsViewPrinters += ' missing';
+                    title = 'Sleep';
+                    break;
+                case 'unreachable':
+                    iconsViewPrinters += ' missing';
+                    title = 'Unreachable';
+                    break;
+                case 'turn off':
+                    iconsViewPrinters += ' missing';
+                    title = 'Turn Off';
+                    break;
+                case 'with alerts':
+                    iconsViewPrinters += ' warning';
+                    title = 'With Alerts';
+                    break;
+                case 'busy with activities':
+                    iconsViewPrinters += ' connected';
+                    title = 'Busy With Activities';
+                    break;
+                case 'with system errors':
+                    iconsViewPrinters += ' error';
+                    title = 'With System Errors';
+                    break;
+                case 'not initialized':
+                    iconsViewPrinters += ' missing';
+                    title = 'Not Initialized';
+                    break;
+                case 'unknown':
+                    iconsViewPrinters += ' missing';
+                    title = 'Unknown';
+                    break;
+                default:
+                    iconsViewPrinters += ' missing';
+                    title = 'Unknown';
+            }
+
+            iconsViewPrinters += ' title="' + title + '">';
+            iconsViewPrinters += '<div class="info">';
+
+            if (printersList[i].basicInfo.ip) {
+                iconsViewPrinters += '<p>' + printersList[i].basicInfo.ip + '</p>';
+            } else {
+                iconsViewPrinters += '<p>&mdash;</p>';
+            }
+
+            if (printersList[i].basicInfo.modelname) {
+                iconsViewPrinters += '<p>' + printersList[i].basicInfo.modelname + '</p>';
+            } else {
+                iconsViewPrinters += '<p>&mdash;</p>';
+            }
+
+            if (printersList[i].detailedInfo.firmwareVersion) {
+                iconsViewPrinters += '<p>' + printersList[i].detailedInfo.firmwareVersion + '</p>';
+            } else {
+                iconsViewPrinters += '<p>&mdash;</p>';
+            }
+
+            iconsViewPrinters += '</div>';
+
+            if (printersList[i].metadata.reservedBy != null && printersList[i].metadata.reservedUntil != null) {
+                iconsViewPrinters += '<div class="state"><p>RESERVED</p></div>';
+            }
+
+            iconsViewPrinters += '</div>';
+        }
+
+        iconsViewPrinters += '</div>';
+        listViewPrinters += '</div>';
+        columnsViewPrinters += '</div>';
+
+        $('#iconsView div.wrapper').remove();
+        $(iconsViewPrinters).appendTo('#iconsView');
+    }
 });
