6e94bfdc8830e8328efb7018e7df00920e69ee7d
Chema Rivera <chema@iMac-de-Chema.local>
2018-02-20 21:02:39 +0100

    printers population into views finished

--- client/js/03_printersPopulation.js
+++ client/js/03_printersPopulation.js
@@ -1,11 +1,14 @@
 'use strict';
 
 $(function() {
+    var printersList;
+
     $.ajax({
         url: 'http://'+serverAddress+':'+serverPort+'/printers/list',
         method: 'GET',
         success: function(res, status) {
-            poblateViews(res);
+            printersList = res;
+            populateViews();
         },
         error: function(jqXHR, status, err) {
             if (!err) {
@@ -19,92 +22,146 @@ $(function() {
     });
 
 
-    function poblateViews(printersList) {
-        var iconsViewPrinters = '<div class="wrapper">';
-        var listViewPrinters = '<table class="wrapper">';
-        var columnsViewPrinters = '<div class="wrapper">';
+    function populateViews() {
+        var iconsViewPrinters = '<div id="iconsViewPopulation" class="wrapper">';
+        var listViewPrinters = '<tbody id="listViewPopulation">';
+        var columnsViewPrinters = '<div id="columnsViewPopulation" class="wrapper">';
         var title = '';
 
         for (var i=0; i<printersList.length; i++) {
             iconsViewPrinters += '<div class="printer';
+            listViewPrinters += '<tr class="printer';
+            columnsViewPrinters += '<div class="printer';
 
             switch (printersList[i].detailedInfo.status.toLowerCase()) {
                 case 'awake':
                     iconsViewPrinters += ' connected';
+                    listViewPrinters += ' connected';
+                    columnsViewPrinters += ' connected';
                     title = 'Awake';
                     break;
                 case 'sleep':
                     iconsViewPrinters += ' missing';
+                    listViewPrinters += ' missing';
+                    columnsViewPrinters += ' missing';
                     title = 'Sleep';
                     break;
                 case 'unreachable':
                     iconsViewPrinters += ' missing';
+                    listViewPrinters += ' missing';
+                    columnsViewPrinters += ' missing';
                     title = 'Unreachable';
                     break;
                 case 'turn off':
                     iconsViewPrinters += ' missing';
+                    listViewPrinters += ' missing';
+                    columnsViewPrinters += ' missing';
                     title = 'Turn Off';
                     break;
                 case 'with alerts':
                     iconsViewPrinters += ' warning';
+                    listViewPrinters += ' warning';
+                    columnsViewPrinters += ' warning';
                     title = 'With Alerts';
                     break;
                 case 'busy with activities':
                     iconsViewPrinters += ' connected';
+                    listViewPrinters += ' connected';
+                    columnsViewPrinters += ' connected';
                     title = 'Busy With Activities';
                     break;
                 case 'with system errors':
                     iconsViewPrinters += ' error';
+                    listViewPrinters += ' error';
+                    columnsViewPrinters += ' error';
                     title = 'With System Errors';
                     break;
                 case 'not initialized':
                     iconsViewPrinters += ' missing';
+                    listViewPrinters += ' missing';
+                    columnsViewPrinters += ' missing';
                     title = 'Not Initialized';
-                    break;
-                case 'unknown':
-                    iconsViewPrinters += ' missing';
-                    title = 'Unknown';
                     break;
                 default:
                     iconsViewPrinters += ' missing';
+                    listViewPrinters += ' missing';
+                    columnsViewPrinters += ' missing';
                     title = 'Unknown';
             }
 
-            iconsViewPrinters += ' title="' + title + '">';
+            iconsViewPrinters += '" title="' + title + '">';
+            listViewPrinters += '">'
+            columnsViewPrinters += '" title="' + title + '">';
+
             iconsViewPrinters += '<div class="info">';
+            columnsViewPrinters += '<div class="info">';
 
             if (printersList[i].basicInfo.ip) {
-                iconsViewPrinters += '<p>' + printersList[i].basicInfo.ip + '</p>';
+                iconsViewPrinters += '<p>' + printersList[i].basicInfo.ip.replace(/\s/g, '&nbsp;') + '</p>';
+                listViewPrinters += '<td>' + printersList[i].basicInfo.ip + '</td>';
+                columnsViewPrinters += '<p>' + printersList[i].basicInfo.ip.replace(/\s/g, '&nbsp;') + '</p>';
             } else {
                 iconsViewPrinters += '<p>&mdash;</p>';
+                listViewPrinters += '<td>&mdash;</td>';
             }
 
             if (printersList[i].basicInfo.modelname) {
-                iconsViewPrinters += '<p>' + printersList[i].basicInfo.modelname + '</p>';
+                iconsViewPrinters += '<p>' + printersList[i].basicInfo.modelname.replace(/\s/g, '&nbsp;') + '</p>';
+                listViewPrinters += '<td>' + printersList[i].basicInfo.modelname + '</td>';
+                columnsViewPrinters += '<p>' + printersList[i].basicInfo.modelname.replace(/\s/g, '&nbsp;') + '</p>';
             } else {
                 iconsViewPrinters += '<p>&mdash;</p>';
+                listViewPrinters += '<td>&mdash;</td>';
             }
 
             if (printersList[i].detailedInfo.firmwareVersion) {
-                iconsViewPrinters += '<p>' + printersList[i].detailedInfo.firmwareVersion + '</p>';
+                iconsViewPrinters += '<p>' + printersList[i].detailedInfo.firmwareVersion.replace(/\s/g, '&nbsp;') + '</p>';
+                listViewPrinters += '<td>' + printersList[i].detailedInfo.firmwareVersion + '</td>';
             } else {
                 iconsViewPrinters += '<p>&mdash;</p>';
+                listViewPrinters += '<td>&mdash;</td>';
             }
 
-            iconsViewPrinters += '</div>';
+            listViewPrinters += '<td class="state">' + title + '</td>';
+            
+            if (printersList[i].metadata.alias) {
+                listViewPrinters += '<td>' + printersList[i].metadata.alias + '</td>';
+            } else {
+                listViewPrinters += '<td>&mdash;</td>';
+            }
 
+            if (printersList[i].metadata.reservedBy) {
+                listViewPrinters += '<td>' + printersList[i].metadata.reservedBy + '</td>';
+            } else {
+                listViewPrinters += '<td>&mdash;</td>';
+            }
+
+            iconsViewPrinters += '</div>';      // info div end
+            columnsViewPrinters += '</div>';    // info div end
+
+            iconsViewPrinters += '<div class="state">';
+            columnsViewPrinters += '<div class="state">';
+            
             if (printersList[i].metadata.reservedBy != null && printersList[i].metadata.reservedUntil != null) {
-                iconsViewPrinters += '<div class="state"><p>RESERVED</p></div>';
+                iconsViewPrinters += '<p>RESERVED</p>';
+                columnsViewPrinters += '<p>R</p>';
             }
 
-            iconsViewPrinters += '</div>';
+            iconsViewPrinters += '</div></div>';    // state div and printer end
+            listViewPrinters += '<tr>';             // printer end
+            columnsViewPrinters += '</div></div>';  // state div and printer end
         }
 
-        iconsViewPrinters += '</div>';
-        listViewPrinters += '</div>';
-        columnsViewPrinters += '</div>';
+        iconsViewPrinters += '</div>';      // iconsViewPopulation end
+        listViewPrinters += '</tbody>';     // listViewPopulation end
+        columnsViewPrinters += '</div>';    // columnsViewPopulation end
 
-        $('#iconsView div.wrapper').remove();
+        $('#iconsViewPopulation').remove();
+        $('#listViewPopulation').remove();
+        $('#columnsViewPopulation').remove();
+
         $(iconsViewPrinters).appendTo('#iconsView');
+        $(listViewPrinters).appendTo('#listView table.wrapper');
+        $(columnsViewPrinters).appendTo('#columnsViewPrintersColumn');
     }
 });
