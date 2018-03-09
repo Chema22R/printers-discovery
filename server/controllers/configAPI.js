4f5ef756458f963fcf4db42e1ac871e89fdd5f6b
Chema Rivera <chema@iMac-de-Chema.local>
2018-03-09 19:49:37 +0100

    modified the config functions to make them synchronous

--- server/controllers/configAPI.js
+++ server/controllers/configAPI.js
@@ -6,18 +6,10 @@ var fs = require('fs');
 exports.getConfigData = function(req, res) {
     var logEntry = 'Get Configuration Data (' + new Date() + ', ' + req.ip + '):';
 
-    fs.readFile('./config.json', {
-        encoding: 'utf8',
-        flag: 'r'
-    }, function(err, data) {
-        if (err) {
-            closeLog('\tError retrieving the server configuration data: ' + err.message, 1);
-            res.sendStatus(500);
-        } else {
-            closeLog('\tConfiguration data successfully retrieved', 3);
-            res.status(200).json(JSON.parse(data));
-        }
-    });
+    var data = fs.readFileSync('./config.json', {encoding: 'utf8', flag: 'r'});
+
+    closeLog('\tConfiguration data successfully retrieved', 3);
+    res.status(200).json(JSON.parse(data));
 
     function closeLog(msj, level) {
         if (level <= req.app.locals.configData.logLevel) {
@@ -34,18 +26,10 @@ exports.getConfigData = function(req, res) {
 exports.updateConfigData = function(req, res) {
     var logEntry = 'Update Configuration Data (' + new Date() + ', ' + req.ip + '):';
 
-    fs.writeFile('./config.json', JSON.stringify(req.body), {
-        encoding: 'utf8',
-        flag: 'w'
-    }, function (err) {
-        if (err) {
-            closeLog('\tError updating the server configuration file: ' + err.message, 1);
-            res.sendStatus(500);
-        } else {
-            closeLog('\tServer configuration file successfully updated', 3);
-            res.sendStatus(200);
-        }
-    });
+    fs.writeFileSync('./config.json', JSON.stringify(req.body), {encoding: 'utf8', flag: 'w'});
+
+    closeLog('\tServer configuration file successfully updated', 3);
+    res.sendStatus(200);
 
     function closeLog(msj, level) {
         if (level <= req.app.locals.configData.logLevel) {
