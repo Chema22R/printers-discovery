d3f625064505de649e11c49f204ae98b0df124d6
Chema Rivera <chema@Chemas-iMac.local>
2019-05-26 20:26:09 +0200

    Added momentum-based scrolling for safari devices and project intro menu updated

--- client/libs/projectIntro.js
+++ client/libs/projectIntro.js
@@ -3,16 +3,6 @@
 document.body.classList.add("noScroll"); // Initial prevent scrolling while project intro is visible
 
 
-/* Project intro message */
-
-const projectIntroText =    "Application with the aim of analyzing the local network in search of prototypes of printers" +
-                            " capturing and storing their information.<br>Through the interface, users can modify the" +
-                            " information of the detected printers and reserve them to be able to use and work freely" +
-                            " with them for a period of time.";
-
-document.getElementById("projectIntroText").innerHTML = projectIntroText;
-
-
 /* Text fade-in animation */
 
 document.getElementById("projectIntroContent").style.maxWidth = document.getElementById("projectIntroImg").offsetWidth + "px";
