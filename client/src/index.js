d63687403663f7feb7d8acd0cea2fa7b743ff4e6
Chema Rivera <chema22r@gmail.com>
2019-08-07 18:40:18 +0200

    Fullcalendar, Timepicker and jQuery UI packages were implemented in the correct way to work with webpack

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,10 @@
 import * as projectIntro from "project-intro";
+import "jquery-ui/ui/widgets/datepicker";
+import "jquery-ui/ui/widgets/slider";
 
+import "./assets/style/jquery-ui.css";
+import "./assets/style/timepicker.css";
+import "./assets/style/fullcalendar.min.css";
 import "./assets/style/normalize.css";
 import "./assets/style/main.css";
 import "./assets/style/headerBar.css";
@@ -8,6 +13,9 @@ import "./assets/style/listView.css";
 import "./assets/style/columnsView.css";
 import "./assets/style/menus.css";
 
+import "./app/timepicker.js";
+import "./app/fullcalendar.min.js";
+import "./app/global.js";
 import "./app/cookies.js";
 import "./app/navigation.js";
 import "./app/printers.js";
@@ -17,85 +25,3 @@ import "./app/config.js";
 
 window.projectIntro = projectIntro;
 projectIntro.init();
-
-
-/* Variables
-========================================================================== */
-
-window.defaultView = 'iconsView';
-
-window.printersPersistent = new Object();
-window.advancedFilters = new Object();
-
-window.basicFilters = {
-    hostname: false,
-    ip: false,
-    modelname: false,
-    firmware: false,
-    status: false,
-    alias: false,
-    location: false,
-    workteam: false,
-    reservedBy: false,
-    notes: false
-};
-
-window.sortingConfig = {
-    param: 'modelname',
-    direction: true
-};
-
-window.listViewHeaders = {
-    hostname: false,
-    ip: true,
-    modelname: true,
-    firmware: true,
-    status: true,
-    creationDate: false,
-    lastUpdateStatus: false,
-    alias: true,
-    location: false,
-    workteam: false,
-    reservedBy: true,
-    reservedUntil: false
-};
-
-window.dateTimePickerOptions = {
-    controlType: 'select',
-    oneLine: true,
-    timeInput: true,
-    dateFormat: 'dd/mm/yy',
-    timeFormat: 'HH:mm'
-};
-
-
-/* Functions
-========================================================================== */
-
-window.compareValues = (var1, var2) => {
-    var mapReplace = {'á':'a', 'é':'e', 'í':'i', 'ó':'o', 'ú':'u', 'à':'a', 'è':'e', 'ì':'i', 'ò':'o', 'ú':'u', 'ä':'a', 'ë':'e', 'ï':'i', 'ö':'o', 'ü':'u'};
-
-    var1 = String(var1).toLowerCase().trim().replace(/\s\s+/g, ' ').replace(/[áéíóúàèìòùäëïöü]/g, function(match) {return mapReplace[match];});
-    var2 = String(var2).toLowerCase().trim().replace(/\s\s+/g, ' ').replace(/[áéíóúàèìòùäëïöü]/g, function(match) {return mapReplace[match];});
-
-    if (var1.includes(var2)) {
-        return true;
-    } else {
-        return false;
-    }
-};
-
-window.showMessage = (msj, color) => {
-    $('#fixedLog p')
-    .text(msj)
-    .css({
-        color: '#FFF',
-        background: color
-    });
-
-    $('#fixedLog').fadeIn('slow', function() {
-        setTimeout(function() {
-            $('#fixedLog').fadeOut('slow');
-        }, 2000);
-    });
-};
