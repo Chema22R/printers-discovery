857f362bcb4a63b3717e7f2052118f1139bdc8fc
Chema <chema22r@gmail.com>
2019-09-04 21:02:30 +0200

    Nodemon-webpack module implemented and some npm scripts modified

--- server/webpack.dev.js
+++ server/webpack.dev.js
@@ -2,6 +2,7 @@ const common = require("./webpack.config.js");
 const merge = require("webpack-merge");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const webpack = require("webpack");
+const NodemonPlugin = require("nodemon-webpack-plugin");
 
 module.exports = merge(common, {
     mode: "development",
@@ -10,8 +11,8 @@ module.exports = merge(common, {
         new CleanWebpackPlugin(),
         new webpack.DefinePlugin({
             DEFAULT_PORT: 8000,
-            DATABASE_NAME: JSON.stringify("printersdiscovery"),
             DEFAULT_DATABASE_URI: JSON.stringify("mongodb://localhost/printersdiscovery")
-        })
+        }),
+        new NodemonPlugin()
     ]
 });
