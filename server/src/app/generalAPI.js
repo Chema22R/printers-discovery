2c0b3a997e4a8aff983b588acf08fdaea31c40c4
Jose Maria Rivera <chema22r@gmail.com>
2019-09-15 21:05:15 +0200

    Added two attempts on status check (#29)

--- server/src/app/generalAPI.js
+++ server/src/app/generalAPI.js
@@ -3,15 +3,28 @@
 exports.checkStatus = function(req, res) {
     var logEntry = 'Check Status (' + new Date() + ', ' + req.ip + '):';
 
-    req.app.locals.db.collection('printers').find({}, {}).toArray(function(err, docs) {
-        if (err) {
-            closeLog('\tDatabase disconnected or error accessing Printers collection: ' + err, 1);
-            res.sendStatus(500);
-        } else {
-            closeLog('\tDatabase connected and Printers collection accessible', 3);
-            res.sendStatus(200);
-        }
-    });
+    checkDatabase(0);
+
+    function checkDatabase(attempt) {
+        req.app.locals.db.collection('printers').find({}, {
+            '_id': 0,
+            'creationDate': 1
+        }).toArray(function(err, docs) {
+            if (err) {
+                if (attempt < 2) {
+                    setTimeout(() => {
+                        checkDatabase(++attempt);
+                    }, 5000);
+                } else {
+                    closeLog('\tDatabase disconnected or error accessing Printers collection: ' + err, 1);
+                    res.sendStatus(500);
+                }
+            } else {
+                closeLog('\tDatabase connected and Printers collection accessible', 3);
+                res.sendStatus(200);
+            }
+        });
+    }
 
     function closeLog(msj, level) {
         if (level <= req.app.locals.configData.logLevel) {
