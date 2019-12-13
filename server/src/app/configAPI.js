d43b62e77bfc8c196d256a40ae8de4acc5b74309
Jose Maria Rivera <chema22r@gmail.com>
2019-12-13 02:17:29 -0800

    049 improve logs (#50)
    * Update index.js
    
    * Update configAPI.js
    
    * Update generalAPI.js
    
    * Update printersAPI.js
    
    * Update printersAPI.js

--- server/src/app/configAPI.js
+++ server/src/app/configAPI.js
@@ -4,19 +4,19 @@ var fs = require('fs');
 
 
 exports.getConfigData = function(req, res) {
-    var logEntry = 'Get Configuration Data (' + new Date() + ', ' + req.ip + '):';
+    var logEntry = 'Get Configuration Data: ';
 
     var data = fs.readFileSync('./config.json', {encoding: 'utf8', flag: 'r'});
 
-    closeLog('\tConfiguration data successfully retrieved', 3);
+    closeLog('Configuration data successfully retrieved', 3);
     res.status(200).json(JSON.parse(data));
 
     function closeLog(msj, level) {
         if (level <= req.app.locals.configData.logLevel) {
             if (level >= req.app.locals.configData.logSeparator) {
-                req.app.locals.logger.log(logEntry + msj);
+                req.app.locals.logger.log(logEntry + msj, {meta: {origin: req.ip}});
             } else {
-                req.app.locals.logger.error(logEntry + msj);
+                req.app.locals.logger.error(logEntry + msj, {meta: {origin: req.ip}});
             }
         }
     }
@@ -24,19 +24,19 @@ exports.getConfigData = function(req, res) {
 
 
 exports.updateConfigData = function(req, res) {
-    var logEntry = 'Update Configuration Data (' + new Date() + ', ' + req.ip + '):';
+    var logEntry = 'Update Configuration Data: ';
 
     fs.writeFileSync('./config.json', JSON.stringify(req.body), {encoding: 'utf8', flag: 'w'});
 
-    closeLog('\tServer configuration file successfully updated', 3);
+    closeLog('Server configuration file successfully updated', 3);
     res.sendStatus(200);
 
     function closeLog(msj, level) {
         if (level <= req.app.locals.configData.logLevel) {
             if (level >= req.app.locals.configData.logSeparator) {
-                req.app.locals.logger.log(logEntry + msj);
+                req.app.locals.logger.log(logEntry + msj, {meta: {origin: req.ip}});
             } else {
-                req.app.locals.logger.error(logEntry + msj);
+                req.app.locals.logger.error(logEntry + msj, {meta: {origin: req.ip}});
             }
         }
     }
