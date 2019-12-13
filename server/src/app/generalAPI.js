acf459a011a5b99c09e5e0c4f34e3408eaae4dec
Jose Maria Rivera <chema22r@gmail.com>
2019-12-13 02:00:51 -0800

    Update generalAPI.js

--- server/src/app/generalAPI.js
+++ server/src/app/generalAPI.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.checkStatus = function(req, res) {
-    var logEntry = 'Check Status (' + new Date() + ', ' + req.ip + '):';
+    var logEntry = 'Check Status: ';
 
     checkDatabase(0);
 
@@ -16,22 +16,22 @@ exports.checkStatus = function(req, res) {
                         checkDatabase(++attempt);
                     }, 5000);
                 } else {
-                    closeLog('\tDatabase disconnected or error accessing Printers collection: ' + err, 1);
+                    closeLog('Database disconnected or error accessing Printers collection', err, 1);
                     res.sendStatus(500);
                 }
             } else {
-                closeLog('\tDatabase connected and Printers collection accessible', 3);
+                closeLog('Database connected and Printers collection accessible', null, 3);
                 res.sendStatus(200);
             }
         });
     }
 
-    function closeLog(msj, level) {
+    function closeLog(msj, err, level) {
         if (level <= req.app.locals.configData.logLevel) {
             if (level >= req.app.locals.configData.logSeparator) {
-                req.app.locals.logger.log(logEntry + msj);
+                req.app.locals.logger.log(logEntry + msj, {meta: {origin: req.ip}});
             } else {
-                req.app.locals.logger.error(logEntry + msj);
+                req.app.locals.logger.error(logEntry + msj, {meta: {origin: req.ip, err: err}});
             }
         }
     }
