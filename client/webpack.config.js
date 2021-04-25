07d68cd40d717921f9f06313acbda509211953e0
Jose Maria Rivera <chema22r@gmail.com>
2021-04-25 23:43:02 +0200

    Update webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -9,8 +9,7 @@ module.exports = {
     },
     output: {
         filename: "[name].[contenthash].js",
-        path: path.resolve(__dirname, "dist"),
-        publicPath: '/'
+        path: path.resolve(__dirname, "dist")
     },
     module: {
         rules: [
