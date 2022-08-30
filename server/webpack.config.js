41e4e2e834fc27f2f9e17ea93b913046e9a0d147
Jose Maria Rivera <chema22r@gmail.com>
2022-08-30 17:44:32 +0200

    dependencies upgraded

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
         index: "./src/index.js"
     },
     output: {
+        clean: true,
         filename: "[name].js",
         path: path.resolve(__dirname, "dist")
     },
--- server/webpack.prod.js
+++ server/webpack.prod.js
@@ -1,13 +1,11 @@
 const common = require("./webpack.config.js");
 const { merge } = require("webpack-merge");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const webpack = require("webpack");
 
 module.exports = merge(common, {
     mode: "production",
     devtool: "source-map",
     plugins: [
-        new CleanWebpackPlugin(),
         new webpack.DefinePlugin({
             DEFAULT_CORS_ORIGIN: JSON.stringify(process.env.PRINTERS_DISCOVERY_CORS_ORIGIN),
             DEFAULT_ENV: JSON.stringify("production"),
